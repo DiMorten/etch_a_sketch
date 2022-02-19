1e2123f02980606b7269c0a4d19d8dffa796246e
DiMorten <cmjorgeandres@gmail.com>
2022-02-19 17:05:18 -0500

    Add prompt with max limit

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
-const container = document.querySelector('#container');
 
-function createGrid() {
-    for (i=0; i<16; i++) {
+
+function createBoard(gridSize) {
+    const container = document.querySelector('#container');
+    for (i=0; i<gridSize; i++) {
         const rowDiv = document.createElement('div');
         rowDiv.setAttribute('class', 'row');
         container.appendChild(rowDiv);
-        for (j=0; j<16; j++) {
+        for (j=0; j<gridSize; j++) {
             const boxDiv = document.createElement('div');
             boxDiv.setAttribute('class', 'box');
             boxDiv.setAttribute('id', 'box_' + j.toString() + '_' + i.toString());
@@ -14,15 +15,46 @@ function createGrid() {
             rowDiv.appendChild(boxDiv);        
         }
     }
+
+    const boxes = document.querySelectorAll('.box');
+
+    boxes.forEach(box => box.addEventListener('mouseover', boxMouseoverCallback));
 }
 
 function boxMouseoverCallback(e) {    
     this.classList.add("box-mouseover");    
 }
 
-createGrid();
+function clearBoard() {
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => box.classList.remove("box-mouseover"));
+}
 
-const boxes = document.querySelectorAll('.box');
+function deleteBoard() {
+    const container = document.querySelector('#container');
+    while (container.firstChild) 
+        container.removeChild(container.lastChild);
+}
 
-boxes.forEach(box => box.addEventListener('mouseover', boxMouseoverCallback));
+function promptBoardSize(msg_base, maxLimit) {
+    let n = 0; 
+    do {
+        n++;
+        msg = (n > 1) ? "You exceeded the maximum limit! \n" + msg_base : msg_base; 
+        gridSize = prompt(msg, "16");
+    }
+    while (gridSize > maxLimit)
+    return gridSize
+}
 
+function resetCallback(e) {
+    clearBoard();
+    gridSize = promptBoardSize("Define new grid size (Maximum limit 100).", 100);
+    deleteBoard();
+    createBoard(gridSize);
+}
+createBoard(16);
+
+
+const button = document.querySelector('button');
+button.addEventListener('click', resetCallback);
